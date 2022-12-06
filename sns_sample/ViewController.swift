0d714a2f45f6cfe6ceca8fdbe45a70e6e4d28671
ajj-k <bouando4ha@docomo.ne.jp>
2022-12-06 10:36:46 +0900

    Initial Commit

---
+++ sns_sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sns_sample
+//
+//  Created by Kiyoshi Ohashi on 2022/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
